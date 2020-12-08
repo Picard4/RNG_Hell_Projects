56d54d66d05b66b05182c7436d6434f691350cc3
Palmar <palmardimarco@gmail.com>
2020-12-08 01:19:25 -0500

    The main menu has been prepared, though it lacks documentation.

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,9 +1,27 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", () => {
+    // constants for the Canvas
+    const canvas = document.getElementsByTagName('canvas')[0];
+    const context = canvas.getContext('2d');
 
-// constants for the Canvas
-const canvas = document.getElementsByTagName('canvas')[0];
-const context = canvas.getContext('2d');
+    canvas.height = 700;
+    canvas.width = 1200;
 
-canvas.height = 700;
-canvas.width = 1000;
+    let gameStatus = {
+        easyMode: false,
+        online: false
+    };
+
+    // Get important HTML elements for the game
+    let startButton = document.getElementById("start-button");
+    let mainMenu = document.getElementById("main-menu");
+
+    // Wait for the user to start the game
+    startButton.addEventListener("click", event => {
+        event.preventDefault();
+
+        mainMenu.style.display = "none";
+        canvas.style.display = "block";
+    });
+});
 
